4b3651cf679dc20e5f9247cb1d162f29052f641c
moreom24 <moreom321@gmail.com>
2026-05-11 19:22:32 -0700

    Add balltree

---
+++ balltree.py
@@ -0,0 +1,45 @@
+import numpy as np
+import os
+from PIL import Image
+import time
+from sklearn.neighbors import BallTree
+
+dataset = "C:/Users/moreo/OneDrive/Desktop/lfw_funneled"
+
+def ballTreeLoad(numOfImages = 1000):
+    images = []
+    labels = []
+    for person in os.listdir(dataset):
+        personPath = os.path.join(dataset, person)
+        
+        if not os.path.isdir(personPath):
+            continue
+        
+        for people in os.listdir(personPath):
+            peoplePath = os.path.join(personPath, people)
+            
+            if people.lower().endswith((".png", ".jpg" ,"jpeg")):
+                try:
+                    image = Image.open(peoplePath).convert("L").resize((64, 64))
+                    vector = np.asarray(image).ravel()
+                    images.append(vector)
+                    labels.append(person)
+                
+                    if len(images) >= numOfImages:
+                        return np.array(images), labels
+                except:
+                    continue
+    return np.array(images), labels
+
+images, labels = ballTreeLoad(4000)
+tree = BallTree(images)
+start = time.perf_counter()
+search = time.perf_counter()
+
+for i in range(len(images)):
+    distance, index = tree.query([images[i]], k = 2)
+    
+end = time.perf_counter()
+totalTime = end - start
+
+print(f"(Ball Tree) Total time to search: {totalTime:.6f} seconds")
